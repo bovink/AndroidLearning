79ff8054e8a655484bc49c927b9037fc06498264
bovink <bovink@sina.com>
2018-01-18 22:34:33 +0800

    绑定动画。

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -2,12 +2,25 @@ package com.bovink.androidlearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.TextView;
+
+import butterknife.BindAnim;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindAnim(R.anim.anim_fade_in)
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        TextView textView = (TextView) findViewById(R.id.tv_hello);
+
+        textView.startAnimation(animation);
     }
 }
