b79a03638eacacd98ed8b14ebaed5c2bebc4f1fe
bovink <bovink@sina.com>
2018-01-29 21:15:03 +0800

    生成样例Activity。

---
+++ app/src/main/java/com/bovink/androidlearning/butterknife/BindAnimActivity.java
@@ -0,0 +1,13 @@
+package com.bovink.androidlearning.butterknife;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * com.bovink.androidlearning.butterknife
+ *
+ * @author bovink
+ * @since 2018/1/29
+ */
+
+public class BindAnimActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/bovink/androidlearning/butterknife/BindIntActivity.java
@@ -0,0 +1,20 @@
+package com.bovink.androidlearning.butterknife;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * com.bovink.androidlearning.butterknife
+ *
+ * @author bovink
+ * @since 2018/1/29
+ */
+
+public class BindIntActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
