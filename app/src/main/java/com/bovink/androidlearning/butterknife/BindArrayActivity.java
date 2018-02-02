74cb2c826029cbd1fd415163acdd421ccee88e3b
bovink@sina.com <261512335wen>
2018-02-02 10:39:46 +0800

    初步完成BindArray测试。

--- app/src/main/java/com/bovink/androidlearning/butterknife/BindArrayActivity.java
+++ app/src/main/java/com/bovink/androidlearning/butterknife/BindArrayActivity.java
@@ -1,12 +1,15 @@
 package com.bovink.androidlearning.butterknife;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.bovink.androidlearning.R;
 
 import butterknife.BindArray;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -21,6 +24,18 @@ public class BindArrayActivity extends AppCompatActivity {
     @BindArray(R.array.week)
     String[] week;
 
+    @BindArray(R.array.month)
+    int[] months;
+
+    @BindArray(R.array.week)
+    CharSequence[] charSequences;
+
+    @BindArray(R.array.colors)
+    TypedArray colors;
+
+    @BindView(R.id.tv_hello)
+    TextView helloTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +43,20 @@ public class BindArrayActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         testBindArray();
+        int index = 0;
+        int color = colors.getColor(index, 0);
+        helloTextView.setTextColor(color);
     }
 
     private void testBindArray() {
 
         for (String day : week) {
             System.out.println("day = " + day);
+        }
+
+        for (int month : months) {
+            System.out.println("month = " + month);
+
         }
     }
 }
