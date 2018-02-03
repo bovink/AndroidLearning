b18eb06a89bf01f40a400be6a57fe75ce0134c0e
bovink <bovink@sina.com>
2018-02-03 16:31:03 +0800

    添加bindevent测试activity。

---
+++ app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnCheckedChangedActivity.java
@@ -0,0 +1,27 @@
+package com.bovink.androidlearning.butterknife.bindevent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bovink.androidlearning.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * com.bovink.androidlearning.butterknife.bindevent
+ *
+ * @author bovink
+ * @since 2018/2/3
+ */
+
+public class OnCheckedChangedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bindevent_ontextchanged);
+        ButterKnife.bind(this);
+    }
+
+}
