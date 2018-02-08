700a83960146b293123ceb676f55c82291b6e6e3
bovink@sina.com <261512335wen>
2018-02-08 10:20:19 +0800

    测试OnClick。

--- app/src/main/java/com/bovink/androidlearning/butterknife/TestActivity.java
+++ app/src/main/java/com/bovink/androidlearning/butterknife/TestActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.bovink.androidlearning.R;
 
+import butterknife.OnClick;
+
 /**
  * @author fox
  * @since 2018/02/08
@@ -17,5 +19,11 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+    }
+
+    @OnClick(R.id.btn_test)
+    void clickBtn() {
+
+        System.out.println("TestActivity.clickBtn");
     }
 }
