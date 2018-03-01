70f7690018b2a360fc18c3ba83ef468ef03bc210
bovink@sina.com <261512335wen>
2018-03-01 17:29:50 +0800

    OnEditorAction。

--- app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnEditorActionActivity.java
+++ app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnEditorActionActivity.java
@@ -1,12 +1,18 @@
 package com.bovink.androidlearning.butterknife.bindevent;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import com.bovink.androidlearning.R;
 
 import butterknife.ButterKnife;
+import butterknife.OnEditorAction;
 
 /**
  * com.bovink.androidlearning.butterknife.bindevent
@@ -24,4 +30,14 @@ public class OnEditorActionActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnEditorAction(R.id.et_test)
+    boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            return true;
+        }
+        return false;
+    }
 }
