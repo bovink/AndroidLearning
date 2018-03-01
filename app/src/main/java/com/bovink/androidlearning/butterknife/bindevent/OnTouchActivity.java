c9302291e8803938c5fafd869001b96f3cb87c52
bovink@sina.com <261512335wen>
2018-03-01 14:14:05 +0800

    OnTouch。

--- app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnTouchActivity.java
+++ app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnTouchActivity.java
@@ -1,12 +1,18 @@
 package com.bovink.androidlearning.butterknife.bindevent;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 import com.bovink.androidlearning.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnTouch;
 
 /**
  * com.bovink.androidlearning.butterknife.bindevent
@@ -17,11 +23,33 @@ import butterknife.ButterKnife;
 
 public class OnTouchActivity extends AppCompatActivity {
 
+    @BindView(R.id.tv_test)
+    TextView testTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bindevent_ontouch);
         ButterKnife.bind(this);
+
+        // 不设置可点击的话，就不会触发手指抬起事件
+        testTextView.setClickable(true);
+    }
+
+    @OnTouch(R.id.tv_test)
+    boolean onTouch(View view, MotionEvent motionEvent) {
+
+        switch (motionEvent.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                view.setBackgroundColor(Color.parseColor("#66CCFF"));
+                break;
+            case MotionEvent.ACTION_UP:
+                view.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 
 }
