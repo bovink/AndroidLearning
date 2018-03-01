d72cf54b5775431cf55158c00f4f69c04ec26768
bovink@sina.com <261512335wen>
2018-03-01 10:41:15 +0800

    OnCheckedChanged。

--- app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnCheckedChangedActivity.java
+++ app/src/main/java/com/bovink/androidlearning/butterknife/bindevent/OnCheckedChangedActivity.java
@@ -3,10 +3,13 @@ package com.bovink.androidlearning.butterknife.bindevent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.bovink.androidlearning.R;
 
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 
 /**
  * com.bovink.androidlearning.butterknife.bindevent
@@ -24,4 +27,29 @@ public class OnCheckedChangedActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnCheckedChanged(R.id.rb_test1)
+    void checkRadio1(RadioButton rb, boolean checked) {
+
+        if (checked) {
+
+            Toast.makeText(this, "1 checked", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Toast.makeText(this, "1 not checked", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @OnCheckedChanged(R.id.rb_test2)
+    void checkRadio2(RadioButton rb, boolean checked) {
+
+        if (checked) {
+
+            Toast.makeText(this, "2 checked", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Toast.makeText(this, "2 not checked", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
