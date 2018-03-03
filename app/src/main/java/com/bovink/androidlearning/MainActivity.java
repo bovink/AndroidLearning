7f8fac1d5af3e87d6f6a0d3764289d414bce129d
bovink <bovink@sina.com>
2018-03-03 22:09:41 +0800

    初步使用DataBinding绑定模型与视图。

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -1,13 +1,19 @@
 package com.bovink.androidlearning;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bovink.androidlearning.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("lilei", "23");
+        binding.setUser(user);
+
     }
 }
