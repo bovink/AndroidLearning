5fbd88997adeb43666a0813cf1cb650f15e09a7f
bovink@sina.com <261512335wen>
2018-03-08 16:54:57 +0800

    reset dependencies.

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -1,20 +1,13 @@
 package com.bovink.androidlearning;
 
-import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.bovink.androidlearning.databinding.ActivityMainBinding;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        User user = new User("lilei", "23");
-        binding.setUser(user);
-        binding.setHandler(new EventHandler());
 
     }
 }
