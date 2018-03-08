9e2c06498c54a649ba8857b177c47c5631e600f3
bovink@sina.com <261512335wen>
2018-03-08 17:30:06 +0800

    创建测试Activity。

---
+++ app/src/main/java/com/bovink/androidlearning/databinding/layoutbinding/FirstActivity.java
@@ -0,0 +1,9 @@
+package com.bovink.androidlearning.databinding.layoutbinding;
+
+/**
+ * @author fox
+ * @since 2018/03/08
+ */
+
+public class FirstActivity {
+}
---
+++ app/src/main/java/com/bovink/androidlearning/databinding/layoutdetails/SecondActivity.java
@@ -0,0 +1,9 @@
+package com.bovink.androidlearning.databinding.layoutdetails;
+
+/**
+ * @author fox
+ * @since 2018/03/08
+ */
+
+public class SecondActivity {
+}
