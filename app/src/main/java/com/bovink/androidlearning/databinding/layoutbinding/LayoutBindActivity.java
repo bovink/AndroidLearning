fa04a4a5649384f69d3028035efd2b4a4598e8f3
bovink@sina.com <261512335wen>
2018-03-13 15:32:44 +0800

    添加Activity两种不同的DataBinding方式。

--- app/src/main/java/com/bovink/androidlearning/databinding/layoutbinding/LayoutBindActivity.java
+++ app/src/main/java/com/bovink/androidlearning/databinding/layoutbinding/LayoutBindActivity.java
@@ -1,9 +1,11 @@
 package com.bovink.androidlearning.databinding.layoutbinding;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.bovink.androidlearning.R;
 import com.bovink.androidlearning.databinding.FirstActBinding;
 import com.bovink.androidlearning.model.Person;
 
@@ -20,9 +22,12 @@ public class LayoutBindActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FirstActBinding binding = FirstActBinding.inflate(getLayoutInflater());
+        // 第一种绑定方式
+        FirstActBinding binding = DataBindingUtil.setContentView(this, R.layout.first_act);
 
-        setContentView(binding.getRoot());
+        // 第二种绑定方式
+//        FirstActBinding binding = FirstActBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
 
         person = new Person();
         person.setName("lilei");
