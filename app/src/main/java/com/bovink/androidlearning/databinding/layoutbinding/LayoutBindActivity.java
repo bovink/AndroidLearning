7a86f4987e4911778ef065bb7dbb4bd0b2a9bb24
bovink@sina.com <261512335wen>
2018-03-13 15:29:18 +0800

    测试绑定数据。

--- app/src/main/java/com/bovink/androidlearning/databinding/layoutbinding/LayoutBindActivity.java
+++ app/src/main/java/com/bovink/androidlearning/databinding/layoutbinding/LayoutBindActivity.java
@@ -3,7 +3,6 @@ package com.bovink.androidlearning.databinding.layoutbinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.bovink.androidlearning.databinding.FirstActBinding;
 import com.bovink.androidlearning.model.Person;
@@ -14,6 +13,7 @@ import com.bovink.androidlearning.model.Person;
  */
 
 public class LayoutBindActivity extends AppCompatActivity {
+
     Person person;
 
     @Override
@@ -21,20 +21,14 @@ public class LayoutBindActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         FirstActBinding binding = FirstActBinding.inflate(getLayoutInflater());
+
         setContentView(binding.getRoot());
+
         person = new Person();
         person.setName("lilei");
         person.setSex("male");
 
         binding.setPerson(person);
 
-        binding.clickTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                person.setName("hanmeimei");
-                person.setSex("female");
-            }
-        });
     }
 }
--- app/src/main/java/com/bovink/androidlearning/model/Person.java
+++ app/src/main/java/com/bovink/androidlearning/model/Person.java
@@ -1,39 +1,32 @@
 package com.bovink.androidlearning.model;
 
 import android.databinding.BaseObservable;
-import android.databinding.Bindable;
-
-import com.bovink.androidlearning.BR;
 
 /**
+ * 普通的JSON对象就能满足DataBinding的数据类型要求
+ *
  * @author fox
  * @since 2018/03/09
  */
 
-public class Person extends BaseObservable{
+public class Person extends BaseObservable {
 
     private String name;
     private String sex;
 
-
-
-    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-        notifyPropertyChanged(BR.name);
     }
 
-    @Bindable
     public String getSex() {
         return sex;
     }
 
     public void setSex(String sex) {
         this.sex = sex;
-        notifyPropertyChanged(BR.sex);
     }
 }
