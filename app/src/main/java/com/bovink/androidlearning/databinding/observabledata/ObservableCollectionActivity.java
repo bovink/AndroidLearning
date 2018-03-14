d8f22846ef4a6e5b2f6922b54e6bfe7e196e3a52
bovink@sina.com <261512335wen>
2018-03-14 15:02:49 +0800

    添加可观察集合。

--- app/src/main/java/com/bovink/androidlearning/databinding/observabledata/ObservableCollectionActivity.java
+++ app/src/main/java/com/bovink/androidlearning/databinding/observabledata/ObservableCollectionActivity.java
@@ -1,8 +1,11 @@
 package com.bovink.androidlearning.databinding.observabledata;
 
+import android.databinding.ObservableArrayMap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.bovink.androidlearning.databinding.ObservableCollectionActBinding;
 
 /**
  * @author fox
@@ -11,8 +14,31 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ObservableCollectionActivity extends AppCompatActivity {
 
+    ObservableArrayMap<String, Object> user = new ObservableArrayMap<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ObservableCollectionActBinding binding = ObservableCollectionActBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        user.put("name", "bruce");
+        user.put("hobby", "hero");
+        user.put("age", 35);
+
+        binding.setUser(user);
+
+        EventHandler handler = new EventHandler();
+        binding.setHandler(handler);
+    }
+
+    public class EventHandler {
+
+        public void changeContent() {
+
+            user.put("hobby", "make money");
+            user.put("age", 38);
+        }
     }
 }
