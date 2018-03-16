305fbcd7e357c02055ea6d73741933431920fdab
bovink@sina.com <261512335wen>
2018-03-16 13:57:00 +0800

    创建StudentView。

---
+++ app/src/main/java/com/bovink/androidlearning/widget/StudentView.java
@@ -0,0 +1,25 @@
+package com.bovink.androidlearning.widget;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+/**
+ * @author fox
+ * @since 2018/03/16
+ */
+
+public class StudentView extends AppCompatTextView {
+
+    public StudentView(Context context) {
+        super(context);
+    }
+
+    public StudentView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StudentView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
