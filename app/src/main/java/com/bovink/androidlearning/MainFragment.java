929cbfcb32129c0ac4df29c796d3de4a40fc4c6f
bovink@sina.com <261512335wen>
2018-03-21 16:56:27 +0800

    创建需要的类。

---
+++ app/src/main/java/com/bovink/androidlearning/MainFragment.java
@@ -0,0 +1,11 @@
+package com.bovink.androidlearning;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author fox
+ * @since 2018/03/21
+ */
+
+public class MainFragment extends Fragment {
+}
---
+++ app/src/main/java/com/bovink/androidlearning/MainViewModel.java
@@ -0,0 +1,9 @@
+package com.bovink.androidlearning;
+
+/**
+ * @author fox
+ * @since 2018/03/21
+ */
+
+public class MainViewModel {
+}
