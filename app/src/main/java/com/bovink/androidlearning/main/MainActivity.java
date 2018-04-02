c5bdb6c5cd0c5001c22510a3f10c3834bcfb8bf2
bovink@sina.com <261512335wen>
2018-04-02 14:41:12 +0800

    Dagger注入Activity。

--- app/src/main/java/com/bovink/androidlearning/main/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/main/MainActivity.java
@@ -1,15 +1,27 @@
 package com.bovink.androidlearning.main;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.bovink.androidlearning.R;
 
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(mContext, "Hello, Dagger", Toast.LENGTH_LONG).show();
     }
 }
