c7cff8d5cac1d6398326ae9efe5fc242ccc2b350
bovink@sina.com <261512335wen>
2018-04-02 14:45:35 +0800

    使用DaggerAppCompatActivity配置。

--- app/src/main/java/com/bovink/androidlearning/main/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/main/MainActivity.java
@@ -2,23 +2,21 @@ package com.bovink.androidlearning.main;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.bovink.androidlearning.R;
 
 import javax.inject.Inject;
 
-import dagger.android.AndroidInjection;
+import dagger.android.support.DaggerAppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DaggerAppCompatActivity {
 
     @Inject
     Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
