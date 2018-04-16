bb77a8c7fd9ce09e4b7d03372b44d6a4d75f33f1
bovink@sina.com <261512335wen>
2018-04-16 16:20:45 +0800

    重新注册用户数据。

--- app/src/main/java/com/bovink/androidlearning/MyService.java
+++ app/src/main/java/com/bovink/androidlearning/MyService.java
@@ -24,12 +24,57 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MyService extends Service {
 
+    private CompositeDisposable disposable;
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("MyService.onStartCommand");
         startForeground(1001, new Notification());
         Intent innerIntent = new Intent(this, MyInnerService.class);
         startService(innerIntent);
+
+
+        disposable = new CompositeDisposable();
+        disposable.add(Completable.complete()
+                .observeOn(Schedulers.io())
+                .subscribeWith(new DisposableCompletableObserver() {
+                    @Override
+                    public void onComplete() {
+
+                        List<String> groupIds = new ArrayList<>();
+                        groupIds.add("yunduo_4002");
+                        groupIds.add("yunduo_single");
+
+                        SocketUser user = new SocketUser();
+                        user.setAppId("yunduo");
+                        user.setGroupIds(groupIds);
+                        user.setUserName("测试用学生2");
+                        user.setUserUid("4012");
+
+                        final SocketManager socketManager = SocketManager.newInstance();
+                        socketManager.setOnSocketConnectListener(new SocketManager.OnSocketConnectListener() {
+                            @Override
+                            public void onSocketConnect() {
+                                socketManager.registerUser(socketManager.getSocketUser());
+                            }
+                        });
+                        socketManager.setSocketUser(user);
+                        socketManager.registerUser(user);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                }));
+
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        System.out.println("MyService.onDestroy");
     }
 
     @Nullable
@@ -40,42 +85,20 @@ public class MyService extends Service {
 
     public static class MyInnerService extends Service {
 
-        private CompositeDisposable disposable;
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
+            System.out.println("MyInnerService.onStartCommand");
             startForeground(1001, new Notification());
             stopForeground(true);
             stopSelf();
 
-            disposable = new CompositeDisposable();
-            disposable.add(Completable.complete()
-                    .observeOn(Schedulers.io())
-                    .subscribeWith(new DisposableCompletableObserver() {
-                        @Override
-                        public void onComplete() {
-
-                            List<String> groupIds = new ArrayList<>();
-                            groupIds.add("yunduo_4002");
-                            groupIds.add("yunduo_single");
-
-                            SocketUser user = new SocketUser();
-                            user.setAppId("yunduo");
-                            user.setGroupIds(groupIds);
-                            user.setUserName("测试用学生2");
-                            user.setUserUid("4012");
-
-                            SocketManager socketManager = SocketManager.newInstance();
-                            socketManager.setSocketUser(user);
-                            socketManager.registerUser(user);
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-
-                        }
-                    }));
-
             return super.onStartCommand(intent, flags, startId);
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            System.out.println("MyInnerService.onDestroy");
         }
 
         @Nullable
