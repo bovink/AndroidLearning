2559c14d252a8fde82d59e166745be223899b87d
bovink <bovink@sina.com>
2018-09-06 23:17:23 +0800

    安装apk

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -2,6 +2,7 @@ package com.bovink.androidlearning;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.content.FileProvider;
@@ -32,22 +33,19 @@ public class MainActivity extends AppCompatActivity {
         String path = Environment.getExternalStorageDirectory() + "/" + "bovink/apk/app-debug.apk";
         System.out.println("path = " + path);
 
-        //apk文件的本地路径
-        File apkfile = new File(path);
-        //会根据用户的数据类型打开android系统相应的Activity。
-        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            uri = Uri.fromFile(new File(path));
+        } else {
+            // 从安卓版本24开始，需要通过FileProvider才能获取文件的Uri
+            uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, new File(path));
+        }
+        Intent intent = new Intent();
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        Uri contentUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, apkfile);
-        intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
-        //设置intent的数据类型是应用程序application
-//        intent.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
-        //为这个新apk开启一个新的activity栈
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        //开始安装
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "application/vnd.android.package-archive");
         startActivity(intent);
-        //关闭旧版本的应用程序的进程
-//        android.os.Process.killProcess(android.os.Process.myPid());
 
     }
 }
