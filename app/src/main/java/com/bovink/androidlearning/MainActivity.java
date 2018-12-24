e880687f911b6b202ebb629b0b4b1d1355658a62
bovink <bovink@sina.com>
2018-12-24 11:34:26 +0800

    合成音频视频。

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bovink.androidlearning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String videoPath = Environment.getExternalStorageDirectory() + File.separator + "AudioRecord/video.mp4";
+        String audioPath = Environment.getExternalStorageDirectory() + File.separator + "AudioRecord/audio.mp3";
+        String dstPath = Environment.getExternalStorageDirectory() + File.separator + "AudioRecord/target.mp4";
+        Compounder compounder = Compounder.createCompounder(videoPath, audioPath, dstPath);
+        compounder.start();
     }
 }
