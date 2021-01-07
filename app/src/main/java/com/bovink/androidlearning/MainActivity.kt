4cc48cb88b156397849d987fa2813b3dd62e440d
bovink <bovink@sina.com>
2021-01-07 11:38:51 +0800

    Android Kotlin开发基础练习

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.bovink.androidlearning
 
-import android.content.res.TypedArray
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -11,35 +10,34 @@ import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     lateinit var textView: TextView
-    lateinit var diceTextView: ImageView
+    lateinit var ivDice1: ImageView
+    lateinit var ivDice2: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textView_dice)
         textView.text = "Roll the Dice";
         val btn: Button = findViewById(R.id.button_click)
-        btn.setOnClickListener { view ->
-
-            println("s")
-            println("s")
-            rollDice()
-
-            val diceNum = (1..6).random()
-            textView.text = diceNum.toString()
-            val dice = when (diceNum) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-            diceTextView.setImageResource(dice)
-        }
+        btn.setOnClickListener { rollDice() }
 
         val count_up: Button = findViewById(R.id.btn_count_up)
         count_up.setOnClickListener { countUp() }
-        diceTextView = findViewById(R.id.iv_dice)
+        ivDice1 = findViewById(R.id.iv_dice)
+        ivDice2 = findViewById(R.id.iv_dice2)
+    }
+
+    private fun getRandomDiceImage(): Int {
+        val num = (1..6).random()
+
+        val image = when (num) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return image
     }
 
     private fun countUp() {
@@ -66,13 +64,17 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceTextView.setImageResource(dice)
+        ivDice1.setImageResource(dice)
 
     }
 
     private fun rollDice() {
 
         Toast.makeText(this, "button click", Toast.LENGTH_SHORT).show()
+        val dice1 = getRandomDiceImage()
+        ivDice1.setImageResource(dice1)
 
+        val dice2 = getRandomDiceImage()
+        ivDice2.setImageResource(dice2)
     }
 }
