309f65a3cb0a0330ed32e8caa9e5890db775d2f6
bovink <bovink@sina.com>
2021-02-08 23:49:40 +0800

    测试Fragment创建

---
+++ app/src/main/java/com/bovink/androidlearning/ExampleFragment.kt
@@ -0,0 +1,11 @@
+package com.bovink.androidlearning
+
+import androidx.fragment.app.Fragment
+
+
+/**
+ * @author bovink
+ * @since 2021/2/8
+ */
+class ExampleFragment : Fragment(R.layout.fragment_fire_missiles) {
+}
