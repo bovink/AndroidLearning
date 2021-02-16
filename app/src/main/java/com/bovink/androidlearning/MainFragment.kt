3ea7197ef9bd34c0c24cea74ff12f02a1ec24a64
bovink <bovink@sina.com>
2021-02-16 14:11:13 +0800

    添加navigation导航

---
+++ app/src/main/java/com/bovink/androidlearning/MainFragment.kt
@@ -0,0 +1,10 @@
+package com.bovink.androidlearning
+
+import androidx.fragment.app.Fragment
+
+/**
+ * @author bovink
+ * @since 2021/2/16
+ */
+class MainFragment :Fragment(R.layout.frag_main) {
+}
