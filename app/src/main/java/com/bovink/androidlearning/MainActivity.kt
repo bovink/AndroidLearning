1907f9942bdc1f023db415c7e2e263ce246d70ea
bovink <bovink@sina.com>
2021-03-03 17:50:44 +0800

    添加IntentFilter测试

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.bovink.androidlearning
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.button2).setOnClickListener(){
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/bovink/androidlearning/ShowActivity.kt
@@ -0,0 +1,16 @@
+package com.bovink.androidlearning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author bovink
+ * @since 2021/3/3
+ */
+class ShowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+    }
+}
