21cfbe355b7afaa869cb6d5a042772311ae1fb5e
bovink <bovink@sina.com>
2021-03-04 16:41:38 +0800

    增加横竖屏转换生命周期测试。

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bovink.androidlearning;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -33,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        });
+
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+
+
 
     }
 
