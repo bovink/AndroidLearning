3726dc1762302c1ff94bb269fdd2afe26b98c396
bovink <bovink@sina.com>
2021-03-14 21:22:18 +0800

    添加ViewPager2测试代码。

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.bovink.androidlearning
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
+private const val NUM_PAGES = 5
+
+private const val MIN_SCALE = 0.85f
+private const val MIN_ALPHA = 0.5f
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.pager)
+        val pageAdapter = SlidePageAdapter(this)
+        viewPager.adapter = pageAdapter
+        viewPager.setPageTransformer(ZoomOutPageTransformer())
+
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    private inner class SlidePageAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment = SlidePageFragment()
+
+    }
+
+
+    private inner class  ZoomOutPageTransformer : ViewPager2.PageTransformer {
+
+        override fun transformPage(view: View, position: Float) {
+            view.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> { // [-Infinity,-1)
+                        // This page is way off-screen to the left.
+                        alpha = 0f
+                    }
+                    position <= 1 -> { // [-1,1]
+                        // Modify the default slide transition to shrink the page as well
+                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+
+                        // Scale the page down (between MIN_SCALE and 1)
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+
+                        // Fade the page relative to its size.
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> { // (1,+Infinity]
+                        // This page is way off-screen to the right.
+                        alpha = 0f
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/bovink/androidlearning/SlidePageFragment.kt
@@ -0,0 +1,13 @@
+package com.bovink.androidlearning
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+/**
+ * @author bovink
+ * @since 2021/3/14
+ */
+class SlidePageFragment : Fragment(R.layout.frag_slidepage) {
+
+}
