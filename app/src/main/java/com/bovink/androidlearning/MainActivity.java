b4339f3c9e8b4a5cdb8397ad3eff619006c5ac3b
bovink <bovink@sina.com>
2021-03-14 14:23:27 +0800

    测试构建时动态生成BuildConfig变量以及应用资源值。

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -2,6 +2,7 @@ package com.bovink.androidlearning;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView t = findViewById(R.id.textView);
+        t.setText(BuildConfig.BUIL_s);
     }
 }
