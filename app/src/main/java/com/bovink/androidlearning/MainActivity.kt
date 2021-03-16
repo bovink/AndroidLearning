670c18113cad1b8479f76460cd5a2d4682921b73
bovink <bovink@sina.com>
2021-03-16 17:16:20 +0800

    测试LiveData与普通 data的区别

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.bovink.androidlearning
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    var num: MutableLiveData<Int> = MutableLiveData()
+    var normalNum: Int = 0
+     var tv:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val numObserver = Observer<Int> { newInt ->
+            println(newInt)
+        }
+
+        num.observe(this, numObserver)
+
+        thread {
+            for (i in 1..5) {
+
+                Thread.sleep(1000)
+                num.postValue(i)
+                normalNum = i
+                println(normalNum)
+                tv?.post { tv?.text = normalNum.toString() }
+            }
+            println("end")
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        tv = null
+    }
+
+    override fun onStart() {
+        super.onStart()
+        tv = findViewById(R.id.textView)
     }
 }
