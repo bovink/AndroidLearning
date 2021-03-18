5e68e3815c279e7d2e0fbb71c861e9b02750094a
bovink <bovink@sina.com>
2021-03-18 15:18:32 +0800

    添加ToolBar

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.bovink.androidlearning
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+        R.id.action_settings -> {
+
+            println("settings")
+            true
+        }
+        R.id.action_favorite -> {
+            println("favorite")
+            true
+        }
+        else -> {
+
+            super.onOptionsItemSelected(item)
+        }
     }
 }
