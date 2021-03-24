c8c4215afa881bfc094841b848bc3e30bcc545e6
bovink <bovink@sina.com>
2021-03-24 17:25:57 +0800

    更新LiveData使用

--- app/src/main/java/com/bovink/androidlearning/TestLiveDataActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/TestLiveDataActivity.kt
@@ -1,11 +1,11 @@
 package com.bovink.androidlearning
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import kotlin.concurrent.thread
 
 /**
  * @author bovink
@@ -13,47 +13,26 @@ import kotlin.concurrent.thread
  */
 class TestLiveDataActivity : AppCompatActivity() {
 
-    var liveData: MutableLiveData<Int> = MutableLiveData()
-    var normalData: Int = 0
-    var liveDataText: TextView? = null
-    var normalDataText: TextView? = null
+    var mNumData: MutableLiveData<Int> = MutableLiveData()
+
+    private lateinit var mDataTextView: TextView
+    private lateinit var mAddNumBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_livedata)
 
-
-        println("Main Thread ${Thread.currentThread().id}")
+        mDataTextView = findViewById(R.id.tv_livedata)
 
         val numObserver = Observer<Int> { newInt ->
-            println("LiveData Thread ${Thread.currentThread().id} LiveData Print $newInt")
-            liveDataText?.post { liveDataText?.text = "LiveData : ${newInt}" }
+            mDataTextView.text = newInt.toString()
         }
 
-        liveData.observe(this, numObserver)
+        mNumData.observe(this, numObserver)
 
-        thread {
-            for (i in 0 until 10) {
-
-                Thread.sleep(1000)
-                liveData.postValue(i)
-                normalData = i
-                println("Normal Thread ${Thread.currentThread().id} Normal print $normalData")
-                normalDataText?.post { normalDataText?.text = "NormalData : ${normalData}" }
-            }
-            println("Thread Terminated")
+        mAddNumBtn = findViewById(R.id.btn_add_num)
+        mAddNumBtn.setOnClickListener {
+            mNumData.value = 100
         }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        liveDataText = null
-        normalDataText = null
-    }
-
-    override fun onStart() {
-        super.onStart()
-        liveDataText = findViewById(R.id.tv_livedata)
-        normalDataText = findViewById(R.id.tv_normaldata)
     }
 }
