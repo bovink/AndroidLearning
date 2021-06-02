04b27cf39758850f5e36f5185b9c5ae4ee154200
bovink <bovink@sina.com>
2021-06-02 22:22:20 +0800

    添加数据绑定实例

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -10,8 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        /**
+         * 绑定的方式有很多种，根据实际选择
+         */
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
                 this, R.layout.activity_main)
+        /**
+         * 访问绑定的数据对象
+         */
+        val person = Person("leil", 12)
+        binding.user = person
+        /**
+         * 访问绑定的视图对象
+         */
         binding.textView.text = "this is from program"
 
         binding.button.setOnClickListener {
---
+++ app/src/main/java/com/bovink/androidlearning/Person.kt
@@ -0,0 +1,9 @@
+package com.bovink.androidlearning
+
+/**
+ * 数据类
+ * @author bovink
+ * @since 2021/6/2
+ */
+data class Person(val name: String, val age: Int) {
+}
