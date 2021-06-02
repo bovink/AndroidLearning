7b80e36a1b05d460f8068ffd70f06dcbdc058fd1
bovink <bovink@sina.com>
2021-06-02 13:36:26 +0800

    添加基础View跳转

---
+++ app/src/main/java/com/bovink/androidlearning/CommonViewActivity.kt
@@ -0,0 +1,18 @@
+package com.bovink.androidlearning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author bovink
+ * @since 2021/6/2
+ */
+class CommonViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_commonview)
+    }
+
+}
--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -1,7 +1,28 @@
 package com.bovink.androidlearning
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_enter)
+        btn.setOnClickListener {
+            enterCommonViewActivity()
+        }
+
+    }
+
+    fun enterCommonViewActivity() {
+        val intent = Intent(this, CommonViewActivity::class.java)
+        startActivity(intent)
+    }
 }
