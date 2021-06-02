71c47cd2ee930bb6779cbd3e51de78646ed87854
bovink <bovink@sina.com>
2021-06-02 16:51:53 +0800

    Activity与Fragment添加数据绑定

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -2,12 +2,20 @@ package com.bovink.androidlearning
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.bovink.androidlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main)
+        binding.textView.text = "this is from program"
+
+        binding.button.setOnClickListener {
+            println("this is from activity program")
+        }
     }
 }
