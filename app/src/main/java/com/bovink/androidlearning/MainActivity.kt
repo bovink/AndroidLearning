b924a0503785dfdfa2e1afc17c64cd208dad318b
bovink <bovink@sina.com>
2021-06-08 11:52:27 +0800

    添加Retrofit基本使用

--- app/src/main/java/com/bovink/androidlearning/MainActivity.kt
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.kt
@@ -2,6 +2,56 @@ package com.bovink.androidlearning
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.Header
+import retrofit2.http.POST
+import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://english.dolphinmedia.cn:8090/dolphin/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(EnglishService::class.java)
+        val res = Res("22427", "1")
+        service.getWordLessonList(res, "MTAwMDA=").also {
+
+            it.enqueue(object : Callback<Any> {
+                override fun onFailure(call: Call<Any>, t: Throwable) {
+                }
+
+                override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                    println("=================")
+                    println(response.body())
+                    println("=================")
+
+                }
+
+
+            })
+        }
+    }
+
+}
+
+data class Res(val childId: String, val resourceId: String)
+
+interface EnglishService {
+    @POST("lesson/queryLessonList")
+    fun getWordLessonList(@Body res: Res,
+                          @Header("token") token: String): Call<Any>
 }
