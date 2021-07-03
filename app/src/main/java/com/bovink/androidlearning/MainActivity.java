be5824e6e7a3552d017fa7ec8c7c77e2748324b7
wuzonggang <wuzonggang@xkw.cn>
2021-07-03 09:18:46 +0800

    ViewFlipper用例

--- app/src/main/java/com/bovink/androidlearning/MainActivity.java
+++ app/src/main/java/com/bovink/androidlearning/MainActivity.java
@@ -2,6 +2,10 @@ package com.bovink.androidlearning;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewFlipper viewFlipper = findViewById(R.id.viewFlipper);
+                viewFlipper.setDisplayedChild((int) (Math.random()*2));
+
+            }
+        });
     }
 }
